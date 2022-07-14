93d871d2d0e22f34f454aa8a4d3c0d25aa48c84e
Jose Munoz <jmunozjr17@gmail.com>
2022-07-14 01:02:26 -0500

    Update script.js

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -14,53 +14,53 @@ const quotes = [
   {
     quote:  "The important thing is that your teammates have to know you're pulling for them and you really want them to be successful.",
     source: 'Kobe Bryant',
-    citation: 'Kobe Bryant Quotes. BrainyQuote.com, BrainyMedia Inc, 2022.',
+    citation: 'Kobe Bryant Quotes | BrainyQuote.com, BrainyMedia Inc, 2022.',
     year: '2017'
   },
  {
     quote:  "Let us always meet each other with smile, for the smile is the beginning of love.",
     source: '-Mother Tersa',
-    citation: '"Mother Teresa Quotes." BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
+    citation: 'Mother Teresa Quotes | BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
     year: '2002'
   },
  {
     quote: "The topic of leadership is a touchy one. A lot of leaders fail because they don't have the bravery to touch that nerve or strike that chord. Throughout my years, I haven't had that fear.",
     source: '-Kobe Bryant',
-    citation: '"Kobe Bryant Quotes." BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
+    citation: 'Kobe Bryant Quotes | BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
     year: '2022'
   },
  {
-    quote: 'Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.',
+    quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
     source: '- Alber Einstein' ,
-    citation: 'Alber Einstein Quotes." BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
+    citation: 'Alber Einstein Quotes | BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
     year: '1909'
  },
 
  {
-  quote: 'Sometimes the questions are complicated, and the answers are simple.',
+  quote: "Sometimes the questions are complicated, and the answers are simple.",
   source: '-Dr.Seuss' ,
-  citation: 'Dr. Seuss Quotes." BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
+  citation: 'Dr. Seuss Quotes | BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
   year: '1919'
 },
 
 {
-  quote: 'There are only two great tragedies in life: one is not getting what you want and the other is getting it.',
-  source: '- Oscar Wilde' ,
-  citation: 'Oscar Wilde Quotes." BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
+  quote: "There are only two great tragedies in life: one is not getting what you want and the other is getting it.",
+  source: 'Oscar Wilde',
+  citation: 'Oscar Wilde Quotes | BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
   year: '1919'
 },
 
 {
-  quote: 'Anyone who has never made a mistake has never tried anything new.',
+  quote: "Anyone who has never made a mistake has never tried anything new.",
   source: 'Alber Einstein' ,
-  citation: 'Alber Einstein Quotes." BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
+  citation: 'Alber Einstein Quotes | BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
   year: '1910'
 },
 
 {
-  quote: 'It’s not that I’m so smart. It’s just that I stay with problems longer.',
+  quote: "It's not that I'm so smart. It's just that I stay with problems longer.",
   source: 'Alber Einstein' ,
-  citation: 'Alber EinsteinQuotes." BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
+  citation: 'Alber EinsteinQuotes | BrainyQuote.com. BrainyMedia Inc, 2022. 12 July 2022.',
   year: '1911'
 },
 ];
@@ -79,15 +79,16 @@ function getRandomQuote () {
 ***/
 function printQuote () {
   let quote = getRandomQuote ();
-  let html = `<p class="quote">${quote.quote}</p>;
-   <p < class="source">${quote.source};
-    if (quote.citation = true) {
-     html =  <span class ="citation">${quote.citation}</span>;
+  let html = `<p class="quote">${quote.quote}</p>`;
+   `<p class="source">${quote.source}`
+    if (quote.citation !== undefined) {
+     html += `<span class ="citation">${quote.citation}</span>`;
     }
-    if (quote.year = true) {
-     html = <span class = "year">${quote.year}</span></p>`;
+    if (quote.year !== undefined) {
+     html += `<span class = "year">${quote.year}</span></p>`;
   return(document.getElementById("quote-box").innerHTML = html);
     }
+  }
 
 /***
  * click event listener for the print quote button
