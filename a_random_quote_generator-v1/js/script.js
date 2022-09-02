b8ff5e4e2db40618b801d7cbe976304c4403ccb6
Jose Munoz <jmunozjr17@gmail.com>
2022-09-01 21:30:44 -0500

    added setInterval

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,7 +10,7 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array of objects
 ***/
-const colors = ['blue', 'teal', 'green', 'orange'];
+const colors = ['red', 'teal', 'green', 'orange', 'purple', 'yellow', 'blue', 'pink', 'babyblue'];
 const quotes = [
   {
     quote:  "The important thing is that your teammates have to know you're pulling for them and you really want them to be successful.",
@@ -95,9 +95,10 @@ function printQuote () {
   }
 
   function getRandomColor () {
-    return colors[Math.floor(Math.random() * 3)]
+    return colors[Math.floor(Math.random() * 8)]
   }
   
+  setInterval(printQuote, 3000);
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
